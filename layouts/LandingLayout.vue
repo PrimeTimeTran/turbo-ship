72712a14359257573c66e151458770c6ff664452
Loi Tran <primetimetran@gmail.com>
2023-11-23 16:58:56 -0500

    Add 2nd landing secion for cards

--- layouts/LandingLayout.vue
+++ layouts/LandingLayout.vue
@@ -1,9 +1,231 @@
 <template>
-  <div>
+  <div class="overflow-auto scrollbar-hide">
     <TheNavbar />
-    <slot />
-    <div>
-      <TheFooter />
-    </div>
+    <section
+      class="w-screen h-screen odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950 flex flex-col pt-24"
+    >
+      <slot />
+    </section>
+    <section
+      id="2"
+      class="w-screen h-screen flex justify-center items-center odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950"
+    > 
+      <div class="flex flex-col text-center justify-center items-middle">
+        
+        <h1 class="text-white text-lg mb-24">sososo</h1>
+        <!-- class="flex flex-col-reverse lg:flex-row justify-center items-center bg-red-200 sm:bg-orange-200 md:bg-blue-200 lg:bg-green-200" -->
+        <div
+          class="flex flex-col-reverse lg:flex-row justify-center items-between"
+        >
+          <div class="flex flex-1 flex-col items-center justify-center p-2">
+            <img
+              src="~/assets/images/Mockups/BrowserEntity.png"
+              class="w-screen lg:max-w-screen-lg lg:relative lg:-left-64"
+            />
+          </div>
+          <div class="grid grid-cols-12 flex-1 flex-row flex-wrap justify-between -mx-4 space-x-1 text-center lg:relative lg:-left-48">
+            <div
+              class="col-span-4 px-4 max-w-full w-full sm:w-1/2 lg:w-full lg:px-2 wow fadeInUp flex flex-row"
+              data-wow-duration="1s"
+              style="
+                visibility: visible;
+                animation-duration: 1s;
+                animation-name: fadeInUp;
+              "
+            >
+              <div
+                class="py-8 px-2 mb-12 bg-gray-50 border-b border-gray-100 hover:shadow-lg rounded-lg transform transition duration-300 ease-in-out hover:-translate-y-2"
+              >
+                <div class="inline-block text-gray-900 mb-4 w-32">
+                </div>
+                <h3
+                  class="text-md leading-normal mb-2 font-semibold text-black"
+                >
+                  SEO Services
+                </h3>
+                <p class="text-gray-500">
+                  This is a wider card with supporting text below as a natural
+                  content.
+                </p>
+              </div>
+              <!-- end service block -->
+            </div>
+            <div
+              class="col-span-4 px-4 max-w-full w-full sm:w-1/2 lg:w-full lg:px-2 wow fadeInUp flex flex-row"
+              data-wow-duration="1s"
+              style="
+                visibility: visible;
+                animation-duration: 1s;
+                animation-name: fadeInUp;
+              "
+            >
+              <div
+                class="py-8 px-2 mb-12 bg-gray-50 border-b border-gray-100 hover:shadow-lg rounded-lg transform transition duration-300 ease-in-out hover:-translate-y-2"
+              >
+                <div class="inline-block text-gray-900 mb-4 w-32">
+                </div>
+                <h3
+                  class="text-md leading-normal mb-2 font-semibold text-black"
+                >
+                  SEO Services
+                </h3>
+                <p class="text-gray-500">
+                  This is a wider card with supporting text below as a natural
+                  content.
+                </p>
+              </div>
+              <!-- end service block -->
+            </div>
+            <div
+              class="col-span-4 px-4 max-w-full w-full sm:w-1/2 lg:w-full lg:px-2 wow fadeInUp flex flex-row"
+              data-wow-duration="1s"
+              style="
+                visibility: visible;
+                animation-duration: 1s;
+                animation-name: fadeInUp;
+              "
+            >
+              <div
+                class="py-8 px-2 mb-12 bg-gray-50 border-b border-gray-100 hover:shadow-lg rounded-lg transform transition duration-300 ease-in-out hover:-translate-y-2"
+              >
+                <div class="inline-block text-gray-900 mb-4 w-32">
+                </div>
+                <h3
+                  class="text-md leading-normal mb-2 font-semibold text-black"
+                >
+                  SEO Services
+                </h3>
+                <p class="text-gray-500">
+                  This is a wider card with supporting text below as a natural
+                  content.
+                </p>
+              </div>
+              <!-- end service block -->
+            </div>
+            <div
+              class="col-span-4 px-4 max-w-full w-full sm:w-1/2 lg:w-full lg:px-2 wow fadeInUp flex flex-row"
+              data-wow-duration="1s"
+              style="
+                visibility: visible;
+                animation-duration: 1s;
+                animation-name: fadeInUp;
+              "
+            >
+              <div
+                class="py-8 px-2  bg-gray-50 border-b border-gray-100 hover:shadow-lg rounded-lg transform transition duration-300 ease-in-out hover:-translate-y-2"
+              >
+                <div class="inline-block text-gray-900 mb-4 w-32">
+                </div>
+                <h3
+                  class="text-md leading-normal mb-2 font-semibold text-black"
+                >
+                  SEO Services
+                </h3>
+                <p class="text-gray-500">
+                  This is a wider card with supporting text below as a natural
+                  content.
+                </p>
+              </div>
+              <!-- end service block -->
+            </div>
+            <div
+              class="col-span-4 px-4 max-w-full w-full sm:w-1/2 lg:w-full lg:px-2 wow fadeInUp flex flex-row"
+              data-wow-duration="1s"
+              style="
+                visibility: visible;
+                animation-duration: 1s;
+                animation-name: fadeInUp;
+              "
+            >
+              <div
+                class="py-8 px-2  bg-gray-50 border-b border-gray-100 hover:shadow-lg rounded-lg transform transition duration-300 ease-in-out hover:-translate-y-2"
+              >
+                <div class="inline-block text-gray-900 mb-4 w-32">
+                </div>
+                <h3
+                  class="text-md leading-normal mb-2 font-semibold text-black"
+                >
+                  SEO Services
+                </h3>
+                <p class="text-gray-500">
+                  This is a wider card with supporting text below as a natural
+                  content.
+                </p>
+              </div>
+              <!-- end service block -->
+            </div>
+            <div
+              class="col-span-4 px-4 max-w-full w-full sm:w-1/2 lg:w-full lg:px-2 wow fadeInUp flex flex-row"
+              data-wow-duration="1s"
+              style="
+                visibility: visible;
+                animation-duration: 1s;
+                animation-name: fadeInUp;
+              "
+            >
+              <div
+                class="py-8 px-2  bg-gray-50 border-b border-gray-100 hover:shadow-lg rounded-lg transform transition duration-300 ease-in-out hover:-translate-y-2"
+              >
+                <div class="inline-block text-gray-900 mb-4 w-32">
+                </div>
+                <h3
+                  class="text-md leading-normal mb-2 font-semibold text-black"
+                >
+                  SEO Services
+                </h3>
+                <p class="text-gray-500">
+                  This is a wider card with supporting text below as a natural
+                  content.
+                </p>
+              </div>
+              <!-- end service block -->
+            </div>
+          </div>
+          <!-- <p class="text-center text-sm uppercase">Turboship</p>
+            <h2 class="text-4xl lg:text-4xl mt-4 text-gray-700 dark:text-white">
+              We do the basics
+              <div
+                class="font-bold animate__animated animate__flash"
+                style="white-space: nowrap"
+              >
+                so you can
+                <span class="border-b-8 border-b-green-400">innovate</span>
+              </div>
+            </h2>
+            <p class="text-xl lg:text-2xl mt-4 text-gray-700 dark:text-white">
+              Focus on what makes your business stand out. Leave the foundation
+              covered
+            </p>
+            <div
+              class="mt-10 flex flex-col justify-center text-center items-center md:flex-row"
+            >
+              <a
+                href="#2"
+                class="mb-3 inline-flex min-h-12 py-2 w-full items-center justify-center rounded bg-green-600 px-6 font-medium lg:text-lg text-white shadow-md transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
+              >
+                Learn more
+              </a>
+              <a
+                href="/"
+                aria-label=""
+                class="mb-3 inline-flex min-h-12 py-2 w-full items-center justify-center rounded px-6 text-white lg:text-lg transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
+              >
+                Read how it works
+              </a>
+            </div> -->
+        </div>
+      </div>
+    </section>
+    <section
+      class="w-screen h-screen odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950"
+    >
+      3
+    </section>
+    <section
+      class="w-screen h-screen odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950"
+    >
+      4
+    </section>
+    <TheFooter />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,51 @@
-<script>
+<script setup>
 definePageMeta({
   layout: 'landing-layout',
 })
 </script>
 <template>
-  <div class="overflow-auto scrollbar-hide">
-    <section
-      class="flex items-center justify-center w-screen h-screen even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-900 dark:text-white"
+  <div class="justify-center items-center lg:pt-12">
+    <!-- class="flex flex-col-reverse lg:flex-row justify-center items-center bg-red-200 sm:bg-orange-200 md:bg-blue-200 lg:bg-green-200" -->
+    <div
+      class="flex flex-col-reverse lg:flex-row justify-center items-center "
     >
-      <div class="flex justify-center items-center md:mx-auto md:pl-24">
+      <div class="flex flex-1 flex-col items-center justify-between text-center space-y-6 p-2 lg:px-8 lg:relative lg:-right-52 lg:text-left lg:items-start">
+        <p class="text-center text-sm uppercase ">Turboship</p>
+        <h2 class="text-4xl lg:text-4xl mt-4 text-gray-700 dark:text-white">
+          We do the basics
+          <div
+            class="font-bold animate__animated animate__flash"
+            style="white-space: nowrap"
+          >
+            so you can
+            <span class="border-b-8 border-b-green-400">innovate</span>
+          </div>
+        </h2>
+        <p class="text-xl lg:text-2xl  mt-4 text-gray-700 dark:text-white">
+          Focus on what makes your business stand out. Leave the foundation
+          covered
+        </p>
         <div
-          class="flex flex-col-reverse space-y-4 md:flex-row md:items-center md:justify-between lg:flex-row"
+          class="mt-10 flex flex-col justify-center text-center items-center md:flex-row"
         >
-          <div class="flex-4 flex-col-reverse">
-            <p class="flex mb-5 text-sm uppercase">Turboship</p>
-            <h2
-              class="mb-6 font-light leading-snug tracking-tight sm:text-6xl sm:leading-snug text-center"
-            >
-              We do basics &
-              <div
-                class="font-bold animate__animated animate__flash"
-                style="white-space: nowrap"
-              >
-                so you can
-                <span class="border-b-8 border-b-green-400">innovate</span>
-              </div>
-            </h2>
-            <p class="text-base text-gray-700 dark:text-white">
-              Focus on what makes your business stand out. Leave the foundation
-              covered
-            </p>
-            <div class="mt-10 flex flex-col items-center md:flex-row">
-              <a
-                href="#2"
-                class="mb-3 inline-flex h-12 w-full items-center justify-center rounded bg-green-600 px-6 font-medium tracking-wide text-white shadow-md transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
-              >
-                Learn more
-              </a>
-              <a
-                href="/"
-                aria-label=""
-                class="group inline-flex items-center font-semibold text-g1"
-              >
-                Read how it works
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="ml-4 h-6 w-6 transition-transform group-hover:translate-x-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div>
-            <img
-              src="~/assets/images/Mockups/BrowserDark.png"
-            />
-          </div>
+          <a
+            href="#2"
+            class="mb-3 inline-flex min-h-12 py-2 w-full items-center justify-center rounded bg-green-600 px-6 font-medium lg:text-lg text-white shadow-md transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
+          >
+            Learn more
+          </a>
+          <a
+            href="/"
+            aria-label=""
+            class="mb-3 inline-flex min-h-12 py-2 w-full items-center justify-center rounded px-6 text-white lg:text-lg transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
+          >
+            Read how it works
+          </a>
         </div>
       </div>
-      <div
-        class="hidden text-9xl varien absolute top-6 left-1/4 text-g/10 z-40"
-      >
-        About Us
+      <div class="flex flex-1 flex-col items-center justify-center p-2">
+        <img src="~/assets/images/Mockups/BrowserAdmin.png" class="w-screen lg:max-w-screen-lg lg:relative lg:-right-52"/>
       </div>
-    </section>
-    <section
-      id="2"
-      class="w-screen h-screen odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950"
-    >
-      2
-    </section>
-    <section class="w-screen h-screen odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950">
-      3
-    </section>
-    <section class="w-screen h-screen odd:bg-red-100 even:bg-green-100 dark:odd:bg-slate-900 dark:even:bg-zinc-950">
-      4
-    </section>
+    </div>
   </div>
-  <ContentDoc class="prose dark:prose-invert" />
 </template>
-<style>
-</style>
