381d380f24eee578e0d3abcbfa1576190a9c6838
Loi Tran <primetimetran@gmail.com>
2024-02-01 06:53:14 -0500

    WIP Navigation tutorial & guide

---
+++ flutter/lib/presentation/features/template.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:turboship/all.dart';
+
+class ProfileSettingsScreen extends StatefulWidget {
+  const ProfileSettingsScreen({super.key});
+
+  @override
+  State<ProfileSettingsScreen> createState() => _ProfileSettingsScreenState();
+}
+
+class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
