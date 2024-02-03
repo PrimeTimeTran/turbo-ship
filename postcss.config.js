4e5554804ad78cb16be99f15e41f4cdd7866eb39
Loi Tran <primetimetran@gmail.com>
2024-02-03 10:26:34 -0500

    Update nuxt mongoose & postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('autoprefixer'),
+    require('tailwindcss'),
+  ],
 }
