3808be1bf0bbb1e54c7bed722a2ab6e5a4c71eb8
primetimetran <primetimetran@gmail.com>
2024-02-19 16:20:51 -0500

    RF for CQ

---
+++ content/Articles/_1.networking/intro.md
@@ -0,0 +1,5 @@
+# Introduction to Networking
+
+The below CIDR table lists all 32 subnet masks for IPv4 addresses, including the number of IP addresses that can be used for hosts.
+
+![CIDR Ranges](https://i.imgur.com/bfiaYUS.png)
--- pages/Login.vue
+++ pages/Login.vue
@@ -1,6 +1,17 @@
+<script setup>
+definePageMeta({
+  layout: 'CustomLayout',
+})
+
+const values = reactive({ email: '', password: '' })
+async function onSubmit(e) {
+  e.preventDefault()
+  console.log(values)
+}
+</script>
+
 <template>
   <div class="flex justify-start items-start h-full bg-black-900 max-h-full">
-    <!-- Left: Image -->
     <div class="w-1/2 h-screen hidden lg:block">
       <img
         alt="Placeholder Image"
@@ -8,69 +19,38 @@
         class="object-fit w-full h-full"
       />
     </div>
-    <!-- Right: Login Form -->
     <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
       <h1 class="dark:text-white text-2xl font-semibold mb-4">Login</h1>
-      <form
-        action="#"
-        method="POST"
-      >
-        <!-- Username Input -->
+      <form @submit="onSubmit">
         <div class="mb-4">
-          <label
-            for="username"
-            class="block text-gray-600 dark:text-white mb-3"
-          >
-            Username
-          </label>
+          <label for="email" class="block text-gray-600 dark:text-white mb-3"> Email </label>
           <input
             type="text"
-            id="username"
-            name="username"
-            class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+            id="email"
+            name="email"
             autocomplete="off"
+            :onchange="(e) => (values.email = e.target.value)"
+            class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           />
         </div>
-        <!-- Password Input -->
         <div class="mb-4">
-          <label
-            for="password"
-            class="block text-gray-600 dark:text-white mb-3"
-          >
-            Password
-          </label>
+          <label for="password" class="block text-gray-600 dark:text-white mb-3"> Password </label>
           <input
             id="password"
             type="password"
             name="password"
             autocomplete="off"
+            :onchange="(e) => (values.password = e.target.value)"
             class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           />
         </div>
-        <!-- Remember Me Checkbox -->
         <div class="mb-4 flex items-center">
-          <input
-            id="remember"
-            type="checkbox"
-            name="remember"
-            class="text-blue-500"
-          />
-          <label
-            for="remember"
-            class="text-gray-600 dark:text-white ml-2"
-          >
-            Remember Me
-          </label>
+          <input id="remember" type="checkbox" name="remember" class="text-blue-500" />
+          <label for="remember" class="text-gray-600 dark:text-white ml-2"> Remember Me </label>
         </div>
-        <!-- Forgot Password Link -->
         <div class="mb-6 text-blue-500">
-          <a
-            href="#"
-            class="hover:underline"
-            >Forgot Password?</a
-          >
+          <a href="#" class="hover:underline">Forgot Password?</a>
         </div>
-        <!-- Login Button -->
         <button
           type="submit"
           class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
@@ -78,20 +58,9 @@
           Login
         </button>
       </form>
-      <!-- Sign up  Link -->
       <div class="mt-6 text-blue-500 text-center">
-        <a
-          href="#"
-          class="hover:underline"
-          >Sign up Here</a
-        >
+        <a href="#" class="hover:underline">Sign up Here</a>
       </div>
     </div>
   </div>
 </template>
-
-<script setup>
-definePageMeta({
-  layout: 'CustomLayout',
-})
-</script>
